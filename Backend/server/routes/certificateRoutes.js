26cf09c7dbeeb736f23743a786470265cc9885bb
Anshika6663999 <anshikabhadauriya69@gmail.com>
2026-03-30 18:23:00 +0530

    Add route to get all certificates

--- Backend/server/routes/certificateRoutes.js
+++ Backend/server/routes/certificateRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import {
+  getCertificates,
   createCert,
   verifyCert,
   downloadCert
@@ -8,8 +9,9 @@ import {
 const router = express.Router();
 
 // IMPORTANT ORDER
-router.post("/");
-router.get("/download/:id", downloadCert);
-router.get("/:id", verifyCert);
+router.post("/", createCert);          // Create new certificate
+router.get("/download/:id", downloadCert); // Download certificate by ID
+router.get("/:id", verifyCert);        // Verify certificate by ID
+router.get("/", getCertificates);      // Get all certificates
 
 export default router;
