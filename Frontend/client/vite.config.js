b1fdafbea24189c7d3d3a730c229cb2d7dde1c53
Anshika6663999 <anshikabhadauriya69@gmail.com>
2026-03-30 18:14:17 +0530

    Add proxy configuration for API calls in Vite

--- Frontend/client/vite.config.js
+++ Frontend/client/vite.config.js
@@ -2,5 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    proxy: {
+      //api calls to backend
+      "/api": {
+        target: process.env.VITE_API_URL, // set in .env
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
