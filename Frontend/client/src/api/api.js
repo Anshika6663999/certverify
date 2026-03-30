3a284a5c07c174340554a37ecb897cd01e72dc76
Anshika6663999 <anshikabhadauriya69@gmail.com>
2026-03-30 18:08:28 +0530

    Use environment variable for API URL

--- Frontend/client/src/api/api.js
+++ Frontend/client/src/api/api.js
@@ -1,8 +1,9 @@
+
 import axios from "axios";
 
-const API = "http://localhost:5000/api/certificates";
+// environment variable
+const API = `${import.meta.env.VITE_API_URL}/api/certificates`;
 
 export const verifyCert = (id) => axios.get(`${API}/${id}`);
-export const downloadCert = (id) =>
-  window.open(`${API}/download/${id}`);
+export const downloadCert = (id) => window.open(`${API}/download/${id}`);
 export const createCert = (data) => axios.post(API, data);
