23561792049be734e5c6ce0e300b13b68ea82121
Anshika6663999 <anshikabhadauriya69@gmail.com>
2026-03-30 18:51:07 +0530

    Update certificateRoutes.js

--- Backend/server/routes/certificateRoutes.js
+++ Backend/server/routes/certificateRoutes.js
@@ -9,9 +9,9 @@ import {
 const router = express.Router();
 
 // IMPORTANT ORDER
-router.post("/", createCert);          // Create new certificate
-router.get("/download/:id", downloadCert); // Download certificate by ID
-router.get("/:id", verifyCert);        // Verify certificate by ID
-router.get("/", getCertificates);      // Get all certificates
+router.post("/", createCert);              // Create new certificate
+router.get("/:id/download", downloadCert); // Download certificate by ID
+router.get("/:id", verifyCert);            // Verify certificate by ID
+router.get("/", getCertificates);          // Get all certificates
 
 export default router;
